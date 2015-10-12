4281cccfb4bdd40e5e3e071549a4ef9e3f505b34
Benjamin Ylvisaker <benjaminy@alumni.cmu.edu>
2015-10-12 11:33:45 -0600

    w4d1

---
+++ InClass/Week4/Day1/url_utils.js
@@ -0,0 +1,40 @@
+function getFormValuesFromURL( url )
+{
+    var kvs = {};
+    var parts = url.split( "?" );
+    if( parts.length === 2 )
+    {
+        var key_value_pairs = parts[1].split( "&" );
+        for( var i = 0; i < key_value_pairs.length; i++ )
+        {
+            var key_value = key_value_pairs[i].split( "=" );
+            kvs[ key_value[0] ] = key_value[1];
+        }
+    }
+    return kvs
+}
+
+function parseCookies( headers )
+{
+    var cookies = {};
+    var hc = headers.cookie;
+    console.log( 'cookies ', hc )
+    hc && hc.split( ';' ).forEach(
+        function( cookie )
+        {
+            var parts = cookie.split( '=' );
+            cookies[ parts.shift().trim() ] =
+                decodeURI( parts.join( '=' ) );
+        } );
+
+    return cookies;
+}
+
+/* return a random int in the range [ low, high ) */
+function randomInt( low, high ) {
+    return Math.floor(Math.random() * (high - low) + low );
+}
+
+exports.getFormValuesFromURL = getFormValuesFromURL;
+exports.parseCookies = parseCookies;
+exports.randomInt = randomInt;
